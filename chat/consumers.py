f1f6c9a7c5d55fa12ff806f96184556765394a60
Igor <igorkorolev2406@gmail.com>
2025-05-04 12:27:12 +0300

    fix bug

--- chat/consumers.py
+++ chat/consumers.py
@@ -8,28 +8,20 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         user = self.scope["user"]
         if user.is_authenticated:
-            print(user)
-            print(self.scope["url_route"]["kwargs"]["name"])
             await self.accept()
-        await self.close()
+        else:
+            await self.close()
 
     async def disconnect(self, close_code):
-        print(close_code)
+        pass
 
     async def receive(self, text_data: str):
-        # text_data_json = json.loads(text_data)
-        # message = text_data_json["message"]
-        # username = text_data_json["username"]
-        # await self.channel_layer.group_send(
-        #     self.roomGroupName, {
-        #         "type": "sendMessage",
-        #         "message": message,
-        #         "username": username,
-        #     })
-        if text_data == 'heartbeat':
-            await self.send(text_data='heartbeat_ack')
-        else:
-            await self.send(text_data="Привіт, WebSocket!")
+        text_data_json = json.loads(text_data)
+        message = text_data_json["message"]
+        username = text_data_json["username"]
+        print(message)
+        await self.send(text_data=json.dumps({"message": message, "username": username}))
+
 
     async def sendMessage(self, event):
         message = event["message"]
