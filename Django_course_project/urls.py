8d3da2761f268efff0125d483256fef4944eaabf
Igor <igorkorolev2406@gmail.com>
2025-05-04 16:33:18 +0300

    upgrade html

--- Django_course_project/urls.py
+++ Django_course_project/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls, name='admin'),
     path("chat/", include("chat.urls"), name="chat"),
     path("", include("user.urls"), name="user"),
 ]
