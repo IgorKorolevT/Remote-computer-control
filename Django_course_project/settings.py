3ba6c74dfc527af04de7e7e4f9a08289578bfdc4
Igor <igorkorolev2406@gmail.com>
2025-05-12 16:21:23 +0300

    change ALLOWED_HOSTS

--- Django_course_project/settings.py
+++ Django_course_project/settings.py
@@ -24,8 +24,8 @@ SECRET_KEY = 'django-insecure-j$5plbwqw2*4kvkrgbha$o4ac_-46q=%u%bm4uve-4az577b!!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = ["34.169.244.15", "localhost", "127.0.0.1"]
-ALLOWED_HOSTS =  ["192.168.50.16", "127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["34.169.244.15", "localhost", "127.0.0.1"]
+# ALLOWED_HOSTS =  ["192.168.50.16", "127.0.0.1", "localhost"]
 # Application definition
 
 INSTALLED_APPS = [
