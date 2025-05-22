c2c92ea6cadb6f2d5092fd8a8dbc5b595ea70676
Igor <igorkorolev2406@gmail.com>
2025-05-22 13:40:31 +0300

    added password reset. add mail backends console. created base_card.html

--- Django_course_project/settings.py
+++ Django_course_project/settings.py
@@ -158,3 +158,13 @@ CHANNEL_LAYERS = {
         "BACKEND": "channels.layers.InMemoryChannelLayer",
     }
 }
+
+# https://docs.djangoproject.com/en/5.1/topics/email/#defining-a-custom-email-backend
+DEFAULT_FROM_EMAIL = "admin@admin.admin"
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+# EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+# EMAIL_HOST = "smtp.gmail.com"
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = "<EMAIL>"
+# EMAIL_HOST_PASSWORD = "<PASSWORD>"
