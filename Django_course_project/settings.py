6d1f6e54e0127eb8817e795c6d4b80b7ac02a36b
Igor <igorkorolev2406@gmail.com>
2025-06-07 12:31:49 +0300

    done email

--- Django_course_project/settings.py
+++ Django_course_project/settings.py
@@ -185,14 +185,14 @@ CHANNEL_LAYERS = {
 # Email
 # https://docs.djangoproject.com/en/5.1/topics/email/#defining-a-custom-email-backend
 INTERNAL_IPS = ["127.0.0.1", "192.168.50.16"]
-DEFAULT_FROM_EMAIL = "admin@admin.admin"
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-# EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-# EMAIL_HOST = "smtp.gmail.com"
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = "<EMAIL>"
-# EMAIL_HOST_PASSWORD = "<PASSWORD>"
+# EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+# DEFAULT_FROM_EMAIL = "admin@admin.admin"
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = "bottulentest@gmail.com"
+EMAIL_HOST_PASSWORD = 'rlnz lrfh nmcd oxvd'
 
 # Rest Framework
 REST_FRAMEWORK = {
