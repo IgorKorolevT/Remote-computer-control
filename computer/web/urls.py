d73ed97fd53fd5cbd8347d57c111e2116dd1c112
Igor <igorkorolev2406@gmail.com>
2026-06-22 10:47:14 +0300

    Created toggle button for monitor computer

--- computer/web/urls.py
+++ computer/web/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 
 from computer.web import views
-
 # app_name = "api"
 
 urlpatterns = [
@@ -9,5 +8,6 @@ urlpatterns = [
     path("<str:name>/detail/", views.ComputerDetailView.as_view(), name="detail"),
     path("<str:name>/update/", views.ComputerUpdateView.as_view(), name="update"),
     path("<str:name>/delete/", views.ComputerDeleteView.as_view(), name="delete"),
+    path("<str:name>/toggle/", views.toggle_computer, name="toggle"),
     path("", views.ComputerListView.as_view(), name="list"),
 ]
