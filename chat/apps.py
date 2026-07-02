e5725d78769895d6c94b19e6fc38bf6ced4c8dfe
Igor <igorkorolev2406@gmail.com>
2026-07-02 15:35:08 +0300

    Fix bug #36

--- chat/apps.py
+++ chat/apps.py
@@ -1,6 +1,22 @@
 from django.apps import AppConfig
+import logging
+from django.db.backends.signals import connection_created
 
+def null_channel_name(sender, connection, **kwargs):
+    # This filed is need ^
+    from computer.models import Computer
+
+    logger = logging.getLogger(__name__)
+
+    try:
+        Computer.objects.update(channel_name=None)
+        logger.debug(f"Set channel_name to none")
+    except Exception as e:
+        logger.error(f"Can't set none channel_name in computers due to:{e}")
 
 class ChatConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "chat"
+
+    def ready(self):
+        connection_created.connect(null_channel_name)
